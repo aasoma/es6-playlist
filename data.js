491b9f72fafcdd439c08b432a1f47d27023dda03
aasoma <asoma2691@myhta.org>
2018-10-16 14:45:17 -1000

    grammar errors fixed

--- data.js
+++ data.js
@@ -27,7 +27,7 @@ const data = [
     {
         artist:'Joey Trap',
         genre:'Rap',
-        song:'I Got 🅱️op In Bakini Bottom',
+        song:'I Got 🅱️op In 🅱️ikini 🅱️ottom',
         image:'https://images.shazam.com/coverart/t364529121-b1256909498_s400.jpg'
     },
     {
